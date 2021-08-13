178b93f21f12b033cdb0976b478cb4977c0f4426
Daniel Muntean <daniel.muntean@keysight.com>
2021-08-13 13:41:51 +0300

    Revert hardcoded wap-user and pass

--- utils/rest_api_wrapper/resources/configuration.py
+++ utils/rest_api_wrapper/resources/configuration.py
@@ -1,4 +1,4 @@
-WAP_USERNAME = 'wap-automation'
-WAP_PASSWORD = 'Y2x0LXdhcC1hdXRvbWF0aW9uOmR1bW15LXdhcC1hdXRvbWF0aW9uLXNlY3JldA=='
+WAP_USERNAME = '<specify your CyPerf username>'
+WAP_PASSWORD = '<specify your CyPerf password>'
 WAP_CLIENT_ID = 'clt-wap'
 TOKEN_ENDPOINT = '/auth/realms/keysight/protocol/openid-connect/token'
