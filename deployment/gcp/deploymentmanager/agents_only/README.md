3f2f8354acfe78228e54a9ca518ce57d0d665b45
kabiswas <kaushik.biswas@keysight.com>
2023-07-28 16:01:46 +0530

    gcp agent only deployment

---
+++ deployment/gcp/deploymentmanager/agents_only/README.md
@@ -0,0 +1,96 @@
+# Deploying the only CyPerf Agent       
+## Introduction
+This solution uses a Python Template to deploy CyPerf Agents in an GCP Cloud.
+There is a new VPC template, meaning the entire necessary resources will be created from scratch, including VPC Network, subnets, Security group etc. 
+See the Template Parameters Section for more details. Each agent has two interfaces. One is Management interface and other is Test interface. Agent communicate with Controller using Management interface. CyPerf test traffic flows through Test interface.
+
+## Topology Diagram
+![cyperf_controller_and_agent_pair](cyperf_controller_and_agent_pair.jpg)
+
+## How to Deploy:
+A configuration File [YAML] and Templates [Python] are used for this Deployment. However, users can also use standalone Python Templates from the gcloud console for this Deployment.
+### Deployment pre-requisites:
+1.	Download following files from OpenIxia.
+2.	From gcp console, open cloud shell window and upload bellow files. 
+- [cyperf_agents_only_new_vpc.py](cyperf_agents_only_new_vpc.py)
+- [cyperf_agents_only_new_vpc.py.schema](cyperf_agents_only_new_vpc.py.schema)
+- [cyperf_agents_only_new_vpc.yaml](cyperf_agents_only_new_vpc.yaml)  
+- [cyperf_agents_only_existing_vpc.py](cyperf_agents_only_existing_vpc.py)
+- [cyperf_agents_only_existing_vpc.py.schema](cyperf_agents_only_existing_vpc.py.schema)
+- [cyperf_agents_only_existing_vpc.yaml](cyperf_agents_only_existing_vpc.yaml) 
+
+### Deployment using Python Template:
+The Deployment Manager requires a Python template and certain parameters to be supplied at command line in the cloud shell.
+The list of exposed parameters is defined in the Template parameter section.
+### Examples of Deployment using Python Template **New VPC**:
+```
+<user>@cloudshell:~ (project name)$ gcloud deployment-manager deployments create <deployment name> --template cyperf_agents_only_new_vpc.py --properties zone:us-east1-c,region:us-east1,agentMachineType:c2-standard-4,agentSourceImage:<Agent  Imagename>,managementNetworkCIDR:<Subnet>,testNetworkCIDR:<Subnet>,agentCount:1
+
+Example:
+
+$ gcloud deployment-manager deployments create keysight-cyperf-gcp --template cyperf_controller_and_agent_pair_new_vpc.py --properties zone:us-east1-c,region:us-east1,agentMachineType:c2-standard-4,agentSourceImage:keysight-cyperf-agent-2-5,managementNetworkCIDR:172.16.5.0/24,testNetworkCIDR:10.0.0.0/8,agentCount:1
+```
+### Examples of Deployment using Python Template **Existing VPC**:
+```
+<user>@cloudshell:~ (project name)$ gcloud deployment-manager deployments create <deployment name> --template cyperf_agents_only_existing_vpc.py --properties zone:us-east1-c,region:us-east1,agentMachineType:c2-standard-4,agentSourceImage:<Agent  Imagename>,management_subnet:<Existing subnet from prior mentioned region and zone>,test_subnet:<Existing subnet from prior mentioned region and zone>,agentCount:1
+
+Example:
+
+$ gcloud deployment-manager deployments create keysight-cyperf-gcp-ext2 --template cyperf_agents_only_existing_vpc.py --properties zone:us-east1-c,region:us-east1,agentMachineType:c2-standard-4,agentSourceImage:keysight-cyperf-agent-2-5,agentCount:1,management_subnetwork:"keysight-cyperf-gcp1-cyperf-management-subnetwork",test_subnetwork:"keysight-cyperf-gcp1-cyperf-test-subnetwork"
+```
+
+### Example of Deployment using a YAML file **New VPC**:
+```
+<user>@cloudshell:~ (project name)$ gcloud deployment-manager deployments create <deployment name> --config cyperf_agents_only_new_vpc.yaml
+```
+
+### Example of Deployment using a YAML file **Existing VPC**:
+```
+<user>@cloudshell:~ (project name)$ gcloud deployment-manager deployments create <deployment name> --config cyperf_agents_only_existing_vpc.yaml
+```
+### SSH Key:
+To generate the public key and enable SSH access to the CyPerf instances, perform the following steps:
+
+1. Create private key and public key, see [Creating a new SSH key](https://cloud.google.com/compute/docs/instances/adding-removing-ssh-keys#createsshkeys).
+2. For New VPC Edit [cyperf_controller_and_agent_pair_new_vpc.py](cyperf_controller_and_agent_pair_new_vpc.py), and specify 
+`sslkey ='<Replace with ssh public key.>'`.
+3. For Existing VPC Edit [cyperf_controller_and_agent_pair_existing_vpc.py](cyperf_controller_and_agent_pair_existing_vpc.py), and specify 
+`sslkey ='<Replace with ssh public key.>'`.
+
+## Template Parameters:
+The following table lists the parameters for this deployment in **New VPC**.
+
+| Parameter label (name)                   | Default            | Description  |
+| ----------------------- | ----------------- | ----- |
+| zone                   | Requires input            | Preferred Zone name for the deployment.  |
+| region                   | Requires input            | Preferred Region name for the deployment.  |
+| agentMachineType                   | c2-standard-4            | Preferred machine Type for CyPerf Agent.  |
+| agentSourceImage                   | keysight-cyperf-agent-2-5            | Preferred CyPerf Agent image.  |
+| managementNetworkCIDR                   | Requires input. Example: 172.16.5.0/24 | This subnet is attached to CyPerf controller and would be used to access the CyPerf controllers' UI & CyPerf agents will use this subnet for control plane communication with controller.  |
+| testNetworkCIDR                   | Requires input. Example: 10.0.0.0/8            | CyPerf agents will use this subnet for test traffic.  |
+| agentCount                  | 1            | Number of CyPerf agents will be deployed from this template.  |
+| serviceAccountEmail         | Requires input       | service accont with 'compute admin' and 'compute network admin' role must be pre exists. Update serviceAccoutEmail value in cyperf_agents_only_new_vpc.py.schema file. Donot use serviceAccountEmail as commandline Parameter. |
+
+## Template Parameters:
+The following table lists the parameters for this deployment in **Existing VPC**.
+
+| Parameter label (name)                   | Default            | Description  |
+| ----------------------- | ----------------- | ----- |
+| zone                   | Requires input            | Preferred Zone name for the deployment.  |
+| region                   | Requires input            | Preferred Region name for the deployment.  |
+| agentMachineType                   | c2-standard-4            | Preferred machine Type for CyPerf Agent.  |
+| agentSourceImage                   | keysight-cyperf-agent-2-5            | Preferred CyPerf Agent image.  |
+| management_subnetwork                   | Requires input. Example: "keysight-cyperf-gcp1-cyperf-management-subnetwork" | This subnet is attached to CyPerf controller and would be used to access the CyPerf controllers' UI & CyPerf agents will use this subnet for control plane communication with controller.  |
+| test_subnetwork                   | Requires input. Example: "keysight-cyperf-gcp1-cyperf-test-subnetwork"    | CyPerf agents will use this subnet for test traffic.  |
+| agentCount                  | 1            | Number of CyPerf agents will be deployed from this template.  |
+| serviceAccountEmail         | Requires input       | service accont with 'compute admin' and 'compute network admin' role must be pre exists. Update serviceAccoutEmail value in cyperf_agents_only_new_vpc.py.schema file. Donot use serviceAccountEmail as commandline Parameter. |
+
+## Post deploy
+
+After successful deployment of stack, flow bellow instructions
+
+-	Go to GCP console and look for the deployed VMs
+-	Select the Controller instance and check the public IP 
+-	Open your browser and access CyPerf Controller UI with URL https://"Controller Public IP" (Default Username/Password: `admin`/`CyPerf&Keysight#1`)
+-   Registered CyPerf agents should appear in Controller UI automatically.
+-   CyPerf license needs to be procured for further usage. These licenses need to be configured at “Administration” followed by “License Manager” on CyPerf controller gear menu.
---
+++ deployment/gcp/deploymentmanager/agents_only/cyperf_agents_only_new_vpc.py
@@ -0,0 +1,297 @@
+#Copyright Keysight Technologies 2021.
+
+#IMPORTANT: If the Software includes one or more computer programs bearing a Keysight copyright notice and in source code format (“Source Files”), such Source Files are subject to the terms and conditions of the Keysight Software End-User License Agreement (“EULA”) www.Keysight.com/find/sweula and these Supplemental Terms. BY USING THE SOURCE FILES, YOU AGREE TO BE BOUND BY THE TERMS AND CONDITIONS OF THE EULA INCLUDING THESE SUPPLEMENTAL TERMS. IF YOU DO NOT AGREE TO THESE TERMS AND CONDITIONS, DO NOT COPY OR DISTRIBUTE THE SOURCE FILES.
+#1.	Additional Rights and Limitations. Keysight grants you a limited, non-exclusive license, without a right to sub-license, to copy and modify the Source Files solely for use with Keysight products, or systems that contain at least one Keysight product. You own any such modifications and Keysight retains all right, title and interest in the underlying Source Files. All rights not expressly granted are reserved by Keysight.
+#2.            General. Capitalized terms used in these Supplemental Terms and not otherwise defined herein shall have the meanings assigned to them in the EULA. To the extent that any of these Supplemental Terms conflict with terms in the EULA, these Supplemental Terms control solely with respect to the Source Files.
+
+def GenerateConfig(context):
+  
+  #varaible definations
+  management_network = context.env['deployment'] + '-cyperf-management-network'
+  
+  management_subnetwork = context.env['deployment'] + '-cyperf-management-subnetwork'
+  
+  management_network_cidr = context.properties['managementNetworkCIDR']
+  
+  test_network = context.env['deployment'] + '-cyperf-test-network'
+  
+  test_subnetwork = context.env['deployment'] + '-cyperf-test-subnetwork'
+  
+  test_network_cidr = context.properties['testNetworkCIDR']
+  
+  region = context.properties['region']
+  
+  zone = context.properties['zone']
+
+  service_account_email  = context.properties['serviceAccountEmail']
+  
+  agent_base_name = context.env['deployment'] + '-cyperf-agent-'
+  
+  controller = context.properties['controllerip']
+
+  sslkey = 'cyperf:' + '<Replace with ssh public key>'
+
+
+
+
+  resources = []
+  
+  ## cyperf Management network
+  resources.append({
+      "name": management_network,
+      "type": "compute.v1.network",
+      "properties": {
+          "autoCreateSubnetworks": False,
+          "routingConfig": {
+              "routingMode": "REGIONAL"
+          },
+          "description": "managemnet network ",
+          "mtu": 1460
+  
+      },
+  })# cyperf Management subnetwork
+  resources.append({
+      "name": management_subnetwork,
+      "type": "compute.v1.subnetwork",
+      "properties": {
+          "enableFlowLogs": False,
+          "ipCidrRange": management_network_cidr,
+          "network": 'global/networks/' + management_network,
+          #'$ref(' + context.env['deployment'] + 'cyperf-management-network' + '.SelfLink)
+          "privateIpGoogleAccess": True,
+          "region": region,
+          "secondaryIpRanges": []
+      },
+      "metadata": {
+          "dependsOn": [
+              management_network,
+              test_network
+          ]
+      },
+  })# cyperf Test Network
+  resources.append({
+      "name": test_network,
+      "type": "compute.v1.network",
+      "properties": {
+          "autoCreateSubnetworks": False,
+          "routingConfig": {
+              "routingMode": "REGIONAL"
+          },
+          "description": "test network ",
+          "mtu": 1460
+  
+      },
+  })# cyperf Test Subnetwork
+  resources.append({
+          "name": test_subnetwork,
+          "type": "compute.v1.subnetwork",
+          "properties": {
+              "enableFlowLogs": False,
+              "ipCidrRange": test_network_cidr,
+              "network": 'global/networks/' + test_network,
+              #'$ref(' + context.env['deployment'] + 'cyperf-test-network' + '.SelfLink)
+              "privateIpGoogleAccess": True,
+              "region": region,
+              "secondaryIpRanges": []
+          },
+          "metadata": {
+              "dependsOn": [
+                  management_network,
+                  test_network
+              ]
+          },
+  
+      },
+  
+  )# cyperf Firewall
+  resources.append({
+      "name": context.env['deployment'] + 'vpc-mngt-firewall',
+      "type": "compute.v1.firewall",
+      "properties": {
+          "network": 'global/networks/' + management_network,
+          #'$ref(' + context.env['deployment'] + 'cyperf-management-network' + '.SelfLink)
+          "priority": 100,
+          "sourceRanges": ["0.0.0.0/0"],
+          "destinationRanges": [],
+  
+          "allowed": [{
+              "IPProtocol": "all"
+          }, ],
+          "direction": "INGRESS",
+          "disabled": False,
+          "enableLogging": False
+      },
+      "metadata": {
+          "dependsOn": [
+              management_network
+  
+  
+          ]
+      },
+  })
+  resources.append({
+      "name": context.env['deployment'] + 'vpc-test-firewall',
+      "type": "compute.v1.firewall",
+      "properties": {
+          "network": 'global/networks/' + test_network,
+          #'$ref(' + context.env['deployment'] + 'test-network' + '.SelfLink)
+          "priority": 100,
+          "sourceRanges": ["0.0.0.0/0"],
+          "destinationRanges": [],
+  
+          "allowed": [{
+                  "IPProtocol": "all"
+              }
+  
+          ],
+          "direction": "INGRESS",
+          "disabled": False,
+          "enableLogging": False
+      },
+      "metadata": {
+          "dependsOn": [
+  
+              test_network
+          ]
+      }
+  })# cyperf Routes
+  resources.append(
+  
+      {
+          "name": context.env['deployment'] + 'default-internet-route-for-mngt',
+          "type": "compute.v1.route",
+          "properties": {
+              "network": 'global/networks/' + management_network,
+              #'$ref(' + context.env['deployment'] + 'cyperf-management-network' + '.SelfLink)
+              "destRange": "0.0.0.0/0",
+              "nextHopGateway": "global/gateways/default-internet-gateway",
+              "priority": 100,
+              "tags": []
+          },
+          "metadata": {
+              "dependsOn": [
+                  management_network
+  
+              ]
+          },
+      }
+  )
+  resources.append({
+      "name": context.env['deployment'] + 'default-internet-route-for-test',
+      "type": "compute.v1.route",
+      "properties": {
+          "network": 'global/networks/' + test_network,
+          #'$ref(' + context.env['deployment'] + 'test-network' + '.SelfLink)
+          "destRange": "0.0.0.0/0",
+          "nextHopGateway": "global/gateways/default-internet-gateway",
+          "priority": 100,
+          "tags": []
+      },
+      "metadata": {
+          "dependsOn": [
+  
+              test_network
+  
+          ]
+      },
+  })
+        
+  
+  # cyperf agents
+  
+  for val in range(1, int(context.properties['agentCount']) + 1):
+    COMPUTE_AGENT_NAME = agent_base_name + str(val)
+    resources.append({
+      "name": COMPUTE_AGENT_NAME,
+      "type": "compute.v1.instance",
+      "properties": {
+          "zone": zone,
+          #"machineType": 'zones/' + zone + '/machineTypes/c2-standard-4',
+          "machineType": 'zones/' + zone + '/machineTypes/' + context.properties['agentMachineType'],
+          "metadata": {
+              "kind": "compute#metadata",
+              "items": [{
+                      "key": "ssh-keys",
+                      "value": sslkey,
+                  }, {
+                      'key': 'startup-script',
+                      'value': ''.join(['#!/bin/bash\n',
+                          'cd /home/cyperf/\n',
+                          'cyperfagent configuration reload\n',
+                          '/bin/bash image_init_gcp.sh %s >> Appsec_init_gcp_log' % controller
+                      ])
+                  }
+  
+              ],
+          },
+          "tags": {
+              "items": ["cyperf-agents"]
+          },
+          "disks": [{
+              "kind": "compute#attachedDisk",
+              "type": "PERSISTENT",
+              "mode": "READ_WRITE",
+              "deviceName": "boot",
+              "boot": True,
+              "autoDelete": True,
+              "initializeParams": {
+                  "sourceImage": 'projects/' + 'kt-nas-cyperf-dev' + '/global/images/' + context.properties['agentSourceImage'],
+                  "diskType": 'zones/' + zone + '/diskTypes/pd-standard',
+                  "diskSizeGb": "10",
+              },
+              "diskEncryptionKey": {},
+          }],
+          "networkInterfaces": [
+  
+              {
+                  "kind": "compute#networkInterface",
+                  "subnetwork": 'regions/' + region + '/subnetworks/' + management_subnetwork,
+                  "accessConfigs": [{
+                      "kind": "compute#accessConfig",
+                      "name": "External NAT",
+                      "type": "ONE_TO_ONE_NAT",
+                      "networkTier": "PREMIUM",
+                  }],
+                  "aliasIpRanges": [],
+              }, {
+                  "kind": "compute#networkInterface",
+                  "subnetwork": 'regions/' + region + '/subnetworks/' + test_subnetwork,
+                  "accessConfigs": [{
+                      "kind": "compute#accessConfig",
+                      "name": "External NAT",
+                      "type": "ONE_TO_ONE_NAT",
+                      "networkTier": "PREMIUM",
+                  }],
+                  "aliasIpRanges": [],
+              },
+          ],
+          "description": "",
+          "labels": {},
+          "scheduling": {
+              "onHostMaintenance": "MIGRATE",
+              "nodeAffinities": [],
+          },
+          "reservationAffinity": {
+              "consumeReservationType": "ANY_RESERVATION"
+          },
+          "serviceAccounts": [{
+              "email": service_account_email,
+              "scopes": ["https://www.googleapis.com/auth/cloud-platform"],
+          }],
+          "shieldedInstanceConfig": {},
+          "confidentialInstanceConfig": {},
+  
+      },
+      "metadata": {
+          "dependsOn": [
+              test_subnetwork,
+              management_subnetwork
+          ],
+  
+      }
+    })
+  
+  #[END use_template_with_variables]
+  return {
+      "resources": resources
+}
