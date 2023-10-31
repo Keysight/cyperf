0cb77151fa3f71e4ed4de81fbb84ec81f934a751
kabiswas <kaushik.biswas@keysight.com>
2023-11-01 03:43:58 +0530

    Updated versions for CyPerf-2.6 release

--- deployment/azure/azureresourcemanager/README.md
+++
@@ -1,1 +0,0 @@
-Azure marketplace image publish still in progress and going through image scaning process.
--- deployment/azure/terraform/azure_b2b_multiple_test_static_ips/azure_agent/output.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-    value = azurerm_linux_virtual_machine.azr_automation_agent.name
-}
-output "private_ip" {
-    value = azurerm_linux_virtual_machine.azr_automation_agent.private_ip_address
-}
-output "public_ip" {
-    value = azurerm_linux_virtual_machine.azr_automation_agent.public_ip_address
-}
