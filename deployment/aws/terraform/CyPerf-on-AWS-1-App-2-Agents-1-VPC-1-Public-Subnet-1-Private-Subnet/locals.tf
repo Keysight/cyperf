9ccb5027d37c70678fb15774cc7a7f1a016aadca
armdupre <a-joseph.dupre@keysight.com>
2024-06-17 09:17:49 -0400

    Initial Checkin.

---
+++ deployment/aws/terraform/CyPerf-on-AWS-1-App-2-Agents-1-VPC-1-Public-Subnet-1-Private-Subnet/locals.tf
@@ -0,0 +1,19 @@
+locals {
+	AgentInstanceType = var.AgentInstanceType
+	Agent1InstanceId = "agent1"
+	Agent2Eth0PrivateIpAddress = "10.0.10.12"
+	Agent2Eth1PrivateIpAddresses = [ "10.0.2.22" ]
+	Agent2InstanceId = "agent2"
+	AppInstanceType = var.AppInstanceType
+	AppTag = "cyperf"
+	InboundIPv4CidrBlocks = var.InboundIPv4CidrBlocks
+	PlacementGroupName = "${local.Preamble}-placement-group-${local.Region}"
+	PlacementGroupStrategy = "cluster"
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
+	PrivateSubnetAvailabilityZone = var.PrivateSubnetAvailabilityZone
+	PublicSubnetAvailabilityZone = var.PublicSubnetAvailabilityZone
+	Region = data.aws_region.current.name
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+}
