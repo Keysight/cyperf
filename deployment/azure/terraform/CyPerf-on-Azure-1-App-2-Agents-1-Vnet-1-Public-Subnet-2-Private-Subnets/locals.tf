21d060bb80ebeeab5d309a55656eb51346da2ab9
armdupre <a-joseph.dupre@keysight.com>
2024-07-09 16:23:45 -0400

    Initial Checkin.

---
+++ deployment/azure/terraform/CyPerf-on-Azure-1-App-2-Agents-1-Vnet-1-Public-Subnet-2-Private-Subnets/locals.tf
@@ -0,0 +1,20 @@
+locals {
+	AgentVmSize = var.AgentVmSize
+	Agent1InstanceId = "agent1"
+	Agent2Eth0IpAddress = "10.0.10.12"
+	Agent2Eth1IpAddresses = ["10.0.3.12"]
+	Agent2InstanceId = "agent2"
+	AppTag = "cyperf"
+	AppVmSize = var.AppVmSize
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.AppTag}"
+	PublicSecurityRuleSourceIpPrefixes = var.PublicSecurityRuleSourceIpPrefixes == null ? [ "${data.http.ip.response_body}/32" ] : var.PublicSecurityRuleSourceIpPrefixes
+	ResourceGroupLocation = var.ResourceGroupLocation
+	ResourceGroupName = var.ResourceGroupName == null ? "${local.Preamble}-resource-group" : var.ResourceGroupName
+	SshKeyAlgorithm = "RSA"
+	SshKeyName = "${local.Preamble}-ssh-key"
+	SshKeyRsaBits = "4096"
+	SubscriptionId = var.SubscriptionId
+	UserEmailTag = var.UserEmailTag == null ? data.azurerm_client_config.current.client_id : var.UserEmailTag
+	UserLoginTag = var.UserLoginTag == null ? "terraform" : var.UserLoginTag
+	UserProjectTag = var.UserProjectTag == null ? random_id.RandomId.id : var.UserProjectTag
+}
---
+++ deployment/azure/terraform/CyPerf-on-Azure-1-App-2-Agents-1-Vnet-1-Public-Subnet-2-Private-Subnets/main.tf
@@ -0,0 +1,69 @@
+module "App" {
+	source = "armdupre/module-cyperf-app/azurerm"
+	version = "3.0.0"
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	VmSize = local.AppVmSize
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.Vnet
+	]
+}
+
+module "Agent1" {
+	source = "armdupre/module-cyperf-agent/azurerm"
+	version = "3.0.0"
+	AppEth0IpAddress = module.App.Instance.private_ip_address
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	Eth1SubnetId = module.Vnet.Private1Subnet.id
+	InstanceId = local.Agent1InstanceId
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	VmSize = local.AgentVmSize
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.App,
+		module.Vnet
+	]
+}
+
+module "Agent2" {
+	source = "armdupre/module-cyperf-agent/azurerm"
+	version = "3.0.0"
+	AppEth0IpAddress = module.App.Instance.private_ip_address
+	Eth0IpAddress = local.Agent2Eth0IpAddress
+	Eth0SubnetId = module.Vnet.PublicSubnet.id
+	Eth1IpAddresses = local.Agent2Eth1IpAddresses
+	Eth1SubnetId = module.Vnet.Private2Subnet.id
+	InstanceId = local.Agent2InstanceId
+	ResourceGroupLocation = azurerm_resource_group.ResourceGroup.location
+	ResourceGroupName = azurerm_resource_group.ResourceGroup.name
+	SshKeyName = azurerm_ssh_public_key.SshKey.name
+	UserEmailTag = local.UserEmailTag
+	UserLoginTag = local.UserLoginTag
+	UserProjectTag = local.UserProjectTag
+	VmSize = local.AgentVmSize
+	depends_on = [
+		azurerm_ssh_public_key.SshKey,
+		module.App,
+		module.Vnet
+	]
+}
+
+resource "azurerm_resource_group" "ResourceGroup" {
+	name = local.ResourceGroupName
+	location = local.ResourceGroupLocation
+}
+
+resource "random_id" "RandomId" {
+	byte_length = 4
+}
