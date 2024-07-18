ac2d8e40abfe89f4cadf88888c659045d57afaaf
armdupre <a-joseph.dupre@keysight.com>
2024-07-18 10:30:12 -0400

    Updated.

--- deployment/aws/terraform/CyPerf-on-AWS-1-App-2-Agents-Add-On/aws.variables.tf
+++ deployment/aws/terraform/CyPerf-on-AWS-1-App-2-Agents-Add-On/aws.variables.tf
@@ -1,12 +1,12 @@
 variable "AwsAccessCredentialsAccessKey" {
-	default = ""
+	default = null
 	description = "Access key component of credentials used for programmatic calls to AWS."
 	sensitive = true
 	type = string
 }
 
 variable "AwsAccessCredentialsSecretKey" {
-	default = ""
+	default = null
 	description = "Secret access key component of credentials used for programmatic calls to AWS."
 	sensitive = true
 	type = string
