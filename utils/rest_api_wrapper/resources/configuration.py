c72fb572841984c25b5776e3087447321c272734
Alex Ilie <alex.ilie@keysight.com>
2024-08-07 17:38:56 +0300

    remove credeantials from configuration module

--- utils/rest_api_wrapper/resources/configuration.py
+++ utils/rest_api_wrapper/resources/configuration.py
@@ -1,5 +1,5 @@
-WAP_USERNAME = 'admin'
-WAP_PASSWORD = 'CyPerf&Keysight#1'
+WAP_USERNAME = ''
+WAP_PASSWORD = ''
 WAP_CLIENT_ID = 'clt-wap'
 TOKEN_ENDPOINT = '/auth/realms/keysight/protocol/openid-connect/token'
 
