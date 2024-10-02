f9f6261264bc6b09ed218c2c099e93b8f887ade4
Alex Ilie <alex.ilie@keysight.com>
2024-10-02 11:06:30 +0300

    shorter version for real time stats script

--- utils/rest_api_wrapper/sample_scripts/stats_during_runtime.py
+++ utils/rest_api_wrapper/sample_scripts/stats_during_runtime.py
@@ -1,10 +1,10 @@
 import sys, time
 sys.path.append("..")
 from lib.REST_WRAPPER import rest, create_new_config, create_traffic_profile, run_test, collect_stats
-
+from pprint import pprint
 
 # CyPerf API test with 1 Traffic profile, 1 App, Throughput objective, custom payload and B2B agents
-test_duration = 60
+test_duration = 30
 create_new_config()
 create_traffic_profile(
     apps=["Portal Chrome to Apache"],
@@ -13,25 +13,20 @@ create_traffic_profile(
     objective_unit="Mbps",
     duration=test_duration,
 )
-rest.set_application_custom_payload(
-    appName="Portal Chrome to Apache",
-    actionName="Upload Image",
-    paramName="Uploaded file",
-    fileName="../resources/payload_file"
-)
 rest.assign_agents()
+rest.set_test_duration(test_duration)
 rest.start_test()
 start_time = time.time()
-real_time_stats = []
+real_time_stats = {}
 while time.time()-start_time < test_duration:
-    real_time_stats.append({})
     for stat in rest.get_available_stats_name():
         real_time_stats[stat] = rest.get_stats_values(statName=stat)
     #take certain actions based on the live stats
     # if time.time()-start_time > test_duration*3/4:
     #     import pdb; pdb.set_trace() 
    
-print(real_time_stats)
+pprint(real_time_stats)
+
 print('Number of read in {} seconds is {}'.format(test_duration,len(real_time_stats)))
 rest.wait_test_finished()
 collect_stats("../test_results", "stats_during_runtime")
