150ffa92dc4022bddd0dc0fbd73f32a9f2f35278
Alex Ilie <alex.ilie@keysight.com>
2024-10-04 15:02:30 +0300

    switch to 5.0 release versions for deployment

--- deployment/aws/terraform/2_vpcs_multiple_ips/variables.tf
+++ deployment/aws/terraform/2_vpcs_multiple_ips/variables.tf
@@ -66,19 +66,19 @@ variable "agent_number"{
 
 variable "mdw_version" {
   type        = string
-  default     = "keysight-cyperf-controller-4-0"
+  default     = "keysight-cyperf-controller-5-0"
   description = "Version for the cyperf controller machine"
 }
 
 variable "broker_version" {
   type        = string
-  default     = "keysight-cyperf-controller-proxy-4-0"
+  default     = "keysight-cyperf-controller-proxy-5-0"
   description = "Version for the controller-proxy machine"
 }
 
 variable "agent_version" {
   type        = string
-  default     = "keysight-cyperf-agent-4-0"
+  default     = "keysight-cyperf-agent-5-0"
   description = "Version for the cyperf agent"
 }
 
@@ -108,6 +108,6 @@ variable "broker_password" {
 
 variable "cyperf_release" {
   type        = string
-  default     = "4.0"
+  default     = "5.0"
   description = "The version of the cyperf release"
 }
