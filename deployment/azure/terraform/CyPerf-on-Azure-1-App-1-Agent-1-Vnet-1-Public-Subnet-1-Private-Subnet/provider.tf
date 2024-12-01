4fa8384ad931cffb62b30c52136ccb49fffc06b7
armdupre <a-joseph.dupre@keysight.com>
2024-12-01 06:22:35 -0500

    Updated.

--- deployment/azure/terraform/CyPerf-on-Azure-1-App-1-Agent-1-Vnet-1-Public-Subnet-1-Private-Subnet/provider.tf
+++ deployment/azure/terraform/CyPerf-on-Azure-1-App-1-Agent-1-Vnet-1-Public-Subnet-1-Private-Subnet/provider.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
-	client_id = var.ClientId
-	client_secret = var.ClientSecret
-	skip_provider_registration = var.SkipProviderRegistration
-	subscription_id = var.SubscriptionId
-	tenant_id = var.TenantId
+	client_id = local.ClientId
+	client_secret = local.ClientSecret
+	resource_provider_registrations = local.ResourceProviderRegistrations
+	subscription_id = local.SubscriptionId
+	tenant_id = local.TenantId
 	features {
 		resource_group {
 			prevent_deletion_if_contains_resources = false
--- deployment/azure/terraform/CyPerf-on-Azure-Boost-1-App-2-Agents-1-Vnet-1-Public-Subnet-1-Private-Subnet/provider.tf
+++ deployment/azure/terraform/CyPerf-on-Azure-Boost-1-App-2-Agents-1-Vnet-1-Public-Subnet-1-Private-Subnet/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
 	client_id = local.ClientId
 	client_secret = local.ClientSecret
-	skip_provider_registration = local.SkipProviderRegistration
+	resource_provider_registrations = local.ResourceProviderRegistrations
 	subscription_id = local.SubscriptionId
 	tenant_id = local.TenantId
 	features {
